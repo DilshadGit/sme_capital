1f6e54d9a96a2e15aa94905f2e6c815145d552cd
Dilshad Abdulla <DilshadGit@users.noreply.github.com>
2017-08-01 10:12:05 +0100

    update create url
    there was a duplication or the create loan and detail loan need to fixed

--- sme_capital/loans_app/loan_api/urls_api.py
+++ sme_capital/loans_app/loan_api/urls_api.py
@@ -11,7 +11,7 @@ from .view_api import (
 
 urlpatterns = [
     url(r'^$', BusinessLoanListAPIView.as_view(), name='list_api'),
-    url(r'^(?P<slug>[\w-]+)/$', BusinessLoanCreateAPIView.as_view(), name='create_api'),
+    url(r'^create/$', BusinessLoanCreateAPIView.as_view(), name='create_api'),
     url(r'^(?P<slug>[\w-]+)/$', BusinessLoanDetailAPIView.as_view(), name='detail_api'),
     url(r'^(?P<slug>[\w-]+)/edit/$', BusinessLoanEditAPIView.as_view(), name='edit_api'),
     url(r'^(?P<slug>[\w-]+)/delete/$', BusinessLoanDeleteAPIView.as_view(), name='delete_api'),
